6ba9d2cc63b9705a980fd61160d0538e6b72a247
Saurabh Jagadale <saurabhjagadale98@gmail.com>
2022-01-10 17:08:39 +0530

    Uploaded Jenkin-Java-Docker

---
+++ src/main/java/com/app/Main.java
@@ -0,0 +1,22 @@
+package com.app;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		
+		System.out.println( checkIfInputIsEvenNumber(120));
+		
+		
+
+	}
+	public static boolean checkIfInputIsEvenNumber(int x) {
+		
+		
+	   return x%2==0;
+	
+	}
+
+}
---
+++ src/test/java/TestMain.java
@@ -0,0 +1,14 @@
+
+import org.junit.jupiter.api.Test;
+
+import com.app.Main;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+public class TestMain {
+
+ @Test
+public void testInputIsEven(){
+ assertTrue(Main.checkIfInputIsEvenNumber(22)); 
+ //Assertion
+ }
+ }
